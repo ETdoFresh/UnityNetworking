437f3050a3345906465aeea9f3470730e412a5ab
ETdoFresh <ETdoFresh@gmail.com>
2019-11-24 11:40:29 -0600

    Add project files.

---
+++ UpdateCSharpNetworking.cmd
@@ -0,0 +1,3 @@
+git remote add CSharpNetworking https://github.com/ETdoFresh/CSharpNetworking.git
+git subtree add --prefix=CSharpNetworking CSharpNetworking master
+git subtree pull --prefix=CSharpNetworking CSharpNetworking master
