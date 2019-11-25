039cbfe99bcf500ac84bf24647dc93eff565d5a2
ETdoFresh <ETdoFresh@gmail.com>
2019-11-24 18:17:40 -0600

    Add WebSocketClientUnity and Example.

---
+++ UnityNetworking/WebSocketClientUnity.cs
@@ -0,0 +1,70 @@
+﻿using CSharpNetworking;
+using System;
+using System.Collections;
+using UnityEngine;
+
+namespace UnityNetworking
+{
+    public class WebSocketClientUnity : MonoBehaviour
+    {
+        public WebSocketClient client;
+        public string uri = "wss://echo.websocket.org";
+
+        public Queue mainThread = Queue.Synchronized(new Queue());
+
+        public UnityEventObject OnOpen = new UnityEventObject();
+        public UnityEventObjectMessage OnMessage = new UnityEventObjectMessage();
+        public UnityEventObject OnClose = new UnityEventObject();
+
+        private void Awake()
+        {
+            client = new WebSocketClient(uri);
+        }
+
+        private void OnEnable()
+        {
+            client.OnOpen += InvokeOnOpen;
+            client.OnMessage += InvokeOnMessage;
+            client.OnClose += InvokeOnClose;
+            client.Open();
+        }
+
+        private void OnDisable()
+        {
+            client.Close();
+            client.OnOpen -= InvokeOnOpen;
+            client.OnMessage -= InvokeOnMessage;
+            client.OnClose -= InvokeOnClose;
+        }
+
+        private void OnDestroy()
+        {
+            client = null;
+        }
+
+        private void Update()
+        {
+            while (mainThread.Count > 0)
+                ((Action)mainThread.Dequeue()).Invoke();
+        }
+
+        private void InvokeOnOpen(object sender, EventArgs e)
+        {
+            mainThread.Enqueue(new Action(() => OnOpen.Invoke(this)));
+        }
+
+        private void InvokeOnMessage(object sender, Message e)
+        {
+            mainThread.Enqueue(new Action(() => OnMessage.Invoke(this, e)));
+        }
+
+        private void InvokeOnClose(object sender, EventArgs e)
+        {
+            mainThread.Enqueue(new Action(() => OnClose.Invoke(this)));
+        }
+
+        public void Send(string message) => client.Send(message);
+
+        public void Send(byte[] bytes) => client.Send(bytes);
+    }
+}
---
+++ UnityNetworkingTestProject/Assets/Scripts/WebSocketClientToInGameConsole.cs
@@ -0,0 +1,44 @@
+﻿using CSharpNetworking;
+using TMPro;
+using UnityEngine;
+using UnityNetworking;
+
+public class WebSocketClientToInGameConsole : MonoBehaviour
+{
+    public TextMeshProUGUI textMesh;
+    public WebSocketClientUnity client;
+
+    void Awake()
+    {
+        client.OnOpen.AddListener(OnOpen);
+        client.OnMessage.AddListener(OnMessage);
+        client.OnClose.AddListener(OnClose);
+    }
+
+    private void OnDestroy()
+    {
+        client.OnOpen.RemoveListener(OnOpen);
+        client.OnMessage.RemoveListener(OnMessage);
+        client.OnClose.RemoveListener(OnClose);
+    }
+
+    private void OnOpen(Object arg0)
+    {
+        textMesh.text += $"Client: Connected to Server!\n";
+    }
+
+    private void OnMessage(Object arg0, Message arg1)
+    {
+        textMesh.text += $"Client: Received from Server: {arg1.data}\n";
+    }
+
+    private void OnClose(Object arg0)
+    {
+        textMesh.text += $"Client: Disconnected!\n";
+    }
+
+    public void OnSend(string message)
+    {
+        textMesh.text += $"<#808080>Client: Sent to Server: {message}</color>\n";
+    }
+}
